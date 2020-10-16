67c5f3ad4acf27ea7b55c96b59344320fc5764de
Lebedev Boris <lebedev.moscow.me@gmail.com>
2020-10-17 00:56:15 +0500

    Base app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,68 +1,18 @@
-import React from 'react';
+import React from 'react'
 import {shallow} from 'enzyme'
-import App, {UnconnectedApp} from './App';
-import {storeFactory} from './../test/testUtils'
+import App from './App'
+import {findByTestAttr} from './../test/testUtils'
+
 
 /**
- * @function setup
- * @param {object} state - State for this setup
- * @returns {ShallowWrapper}
+ * Setup function for app component
  */
-
-const setup = (state={}) => {
-  const store = storeFactory(state)
-  const wrapper = shallow(<App store={store} />).dive().dive()
-  return wrapper
+const setup = () => {
+  return shallow(<App />)
 }
 
-describe('redux properties', () => {
-  test('has access to `success` state', () => {
-    const success = true
-    const wrapper = setup({success})
-    const successProp = wrapper.instance().props.success
-    expect(successProp).toBe(success)
-  })
-
-  test('has access to `secretWord` state', () => {
-    const secretWord = 'party'
-    const wrapper = setup({secretWord})
-    const secretWordProp = wrapper.instance().props.secretWord
-    expect(secretWordProp).toBe(secretWord)
-  })
-
-  test('has access to `guessedWords` state', () => {
-    const guessedWords = [{guessedWord: 'train', letterMatchCount: 3}]
-    const wrapper = setup({guessedWords})
-    const guessedWordsProp = wrapper.instance().props.guessedWords
-    expect(guessedWordsProp).toEqual(guessedWords)
-  })
-
-  test('`getSecretWord` action creator is a function on the props', () => {
-    const wrapper = setup()
-    const getSecretWordProp = wrapper.instance().props.getSecretWord
-    expect(getSecretWordProp).toBeInstanceOf(Function)
-  })
-
-})
-
-test('`getSecretWord` runs on App mount', () => {
-  const getSecretWordMock = jest.fn()
-
-  const props = {
-    getSecretWord: getSecretWordMock,
-    success: false,
-    guessedWords: []
-  }
-
-  // set up app component with getSecretWordMock as the getSecretWord prop
-  const wrapper = shallow(<UnconnectedApp {...props}/>)
-
-  // run lifecycle method
-
-  wrapper.instance().componentDidMount()
-
-  // check to see if mock run
-  const getSecretWordCallCount = getSecretWordMock.mock.calls.length
-
-  expect(getSecretWordCallCount).toBe(1)
+test('App renders without error', () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+  expect(component.length).toBe(1)
 })
